e24caabe378a3e3f1e2f193a8e2e39c399de80d3
calvinkranig <calvinkranig@gmail.com>
2021-10-03 18:29:40 -0400

    finished Game State

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -67,6 +67,17 @@ class GameState():
     self.next_player = next_player
     self.previous_state = previous
     self.last_move = move
+  
+  @property
+  def situation(self):
+    return (self.next_player, self.board)
+    
+  @classmethod
+  def new_game(cls, board_size):
+    if isinstance(board_size, int):
+      board_size = (board_size, board_size)
+    board = Board(*board_size)
+    return GameState(board, Player.black, None, None)
     
   def apply_move(self, move):
     if move.is_play:
@@ -75,13 +86,45 @@ class GameState():
     else:
       next_board = self.board
     return GameState(next_board, self.next_player.other, self, move)
-  
-  @classmethod
-  def new_game(cls, board_size):
-    if isinstance(board_size, int):
-      board_size = (board_size, board_size)
-    board = Board(*board_size)
-    return GameState(board, Player.black, None, None)
+    
+  def does_move_violate_ko(self, player, move):
+    if not move.is_play:
+      return False
+    next_board = copy.deepcopy(self.board)
+    next_board.place_stone(player, move.point)
+    next_situation = (player.other, next_board)
+    past_state = self.previous_state
+    while past_state is not None:
+      if past_state.situation == next_situation:
+        return True
+      past_state = past_state.previous_state
+    return False
+    
+  def is_move_self_capture(self, player, move):
+    if not move.is_play:
+      return False
+    next_board = copy.deepcopy(self.board)
+    next_board.place_stone(player, move.point)
+    new_string = next_board.get_go_string(move.point)
+    return new_string.num_liberties == 0
+    
+  def is_over(self):
+    if self.last_move is None or self.previous_state.last_move is None:
+      return False
+    if (self.last_move.is_resign 
+      or (self.last_move.is_pass and self.previous_state.last_move.is_pass)):
+      return True
+      
+  def is_valid_move(self, move):
+    if self.is_over():
+      return False
+    if move.is_pass or move.is_resign:
+      return True
+    return (
+      self.board.get(move.point) is None
+      and not self.is_move_self_capture(self.next_player,move)
+      and not self.does_move_violate_ko(self.next_player,move)
+    )
 
 class GoString():
   def __init__(self, color, stones, liberties):
